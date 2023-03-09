1745eee479400e9ebef82ddb22cf7edb16495189
Denis <stychynskidenis@gmail.com>
2023-03-09 13:39:36 +0300

    Дополнили Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 1000)
+{
+    int numLast = number % 10;
+
+    Console.WriteLine($"{numLast}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
