9930c1a1fbfcf94cfa0a0eddcc13d66379ed15b5
Denis <stychynskidenis@gmail.com>
2023-03-09 14:50:23 +0300

    Выполнили HomeWork6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= number)
+{
+    if (number % 2 == 0)
+    {
+        
+        Console.Write("Да");
+
+    }
+    if (number % 2 == 1)
+    {
+        
+        Console.Write("Нет");
+
+    }
+        count++;
+
+}
+if (number == 1)
+{
+
+    Console.Write("нет");
+
+}
+
+if (number == 0)
+{
+    Console.Write("нет");
+
+
+}
