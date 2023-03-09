634c146fed5e0cbc70b77e4fa48e2f52f29a10cd
Denis <stychynskidenis@gmail.com>
2023-03-09 13:17:30 +0300

    Добавили Task01

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. На вход принимает число
+// 2. выдает его квадрат ( число умноженное на само себя)
+
+//string input = Console.ReadLine();
+//int number = Convert.ToInt32(input)
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+
+ Console.WriteLine($"Квадрат числа {number} ={square}");
+
---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine($"Квадрат числа {b} ={a} -> Да");
+}
+else
+{
+    Console.WriteLine($"Квадрат числа {b} не равняется {a} -> Нет ");
+}
+
+
