f05b74d13b0c8a485f3c0fcf6065a3ce696d1144
Denis <stychynskidenis@gmail.com>
2023-03-09 15:06:41 +0300

    Выполнили HomeWork8

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (number == 1) Console.WriteLine("Нет четных чисел");
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ",");
+    }
+    
+    count++;
+
+
+}
